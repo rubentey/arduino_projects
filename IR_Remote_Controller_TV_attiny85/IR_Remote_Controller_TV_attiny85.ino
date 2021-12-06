afc1a4ab3ad353092e7a761312a918ea0ca3005e
rubentey <65304294+rubentey@users.noreply.github.com>
2021-12-07 00:02:41 +0100

    1a version attiny85 IR sender to TV
    1a version attiny85 IR sender to TV

---
+++ IR_Remote_Controller_TV_attiny85/IR_Remote_Controller_TV_attiny85.ino
@@ -0,0 +1,63 @@
+#include <IRremote.h>
+
+IRsend irsend;
+
+const int on_off  = 0;
+const int vol_up  = 1;
+const int vol_down  = 2;
+const int input_sig  = 3;
+const int bright_scr  = 4;
+
+int timer;
+int modeCounter = 0;
+
+void wakeUp() {
+  timer = 0;
+}
+
+void setup() {
+  pinMode(on_off, INPUT);
+  pinMode(vol_up, INPUT);
+  pinMode(vol_down, INPUT);
+  pinMode(input_sig, INPUT);
+  pinMode(bright_scr, INPUT);
+}
+
+void loop() {
+  attachInterrupt(0, wakeUp, HIGH);
+  while (timer < 10000) {
+    if (digitalRead(on_off) == HIGH) {
+      timer = 0;
+      delay(50);
+      irsend.sendNEC(0x1000C, 20);
+    }
+
+    if (digitalRead(vol_up) == HIGH) {
+      timer = 0;
+      delay(50);
+      irsend.sendNEC(0x10010, 20);
+    }
+
+    if (digitalRead(vol_down) == HIGH) {
+      timer = 0;
+      delay(50);
+      irsend.sendNEC(0x10011, 20);
+    }
+
+    if (digitalRead(input_sig) == HIGH) {
+      timer = 0;
+      delay(50);
+      irsend.sendNEC(0x10038, 20);
+    }
+
+    if (digitalRead(bright_scr) == HIGH) {
+      timer = 0;
+      delay(50);
+      irsend.sendNEC(0x100F3, 20);
+    }
+    
+    delay(1);
+    timer = timer + 1;
+
+  }
+}
