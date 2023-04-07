ae777a730bac824ac2ba9ed7b541b7a456e46872
rubentey <65304294+rubentey@users.noreply.github.com>
2023-04-07 19:30:28 +0200

    Add files via upload

---
+++ ATTiny85_testing_and_trying/ATTiny85_with_OLED_with_thermocouple_MAX6675/ATTiny85_with_OLED_with_thermocouple_MAX6675.ino
@@ -0,0 +1,27 @@
+#include "max6675.h"
+int tcpDO = 1;  //SPI for thermocouple MAX6675 settings
+int tcpCS = 3;
+int tcpCLK = 4;
+MAX6675 tcp(tcpCLK, tcpCS, tcpDO);
+
+#include <U8x8lib.h>  //For use OLED display
+#include <TinyWireM.h>  //For use I2C in ATTiny
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 2, /* data=*/ 0, /* reset=*/ U8X8_PIN_NONE);
+
+void setup() {
+  TinyWireM.begin();
+  u8x8.begin();
+  u8x8.setPowerSave(0);
+  u8x8.setFont(u8x8_font_7x14B_1x2_r);
+  delay(500);
+}
+
+void loop() {
+  u8x8.setCursor(0,0);
+  u8x8.print("C = ");             // Celsius
+  u8x8.print(tcp.readCelsius());
+  u8x8.setCursor(0,4);
+  u8x8.print("F = ");             // Fahrenheit
+  u8x8.print(tcp.readFahrenheit());
+  delay(500);
+}
